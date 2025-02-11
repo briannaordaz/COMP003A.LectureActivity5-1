1d2664592a5a11b598fa6c03dab571536418b873
briannaordaz <briannaordaz5@gmail.com>
2025-02-10 22:02:52 -0800

    Did steps 1, 2, and 3

---
+++ COMP003A.LectureActivity5-1/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace COMP003A.LectureActivity5_1;
+
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Car myCar = new Car(); // Create a new Car object
+            myCar.Make = "Toyota"; // Set the car's make
+            myCar.Model = "Supra"; // Set the car's model
+            myCar.Year = 2024; // Set the car's year
+            
+            myCar.DisplayInfo(); // Display the car's information
+        }
+
+        internal class Car
+        {
+            //Fields
+            public string Make;
+            public string Model;
+            public int Year;
+
+            /// <summary>
+            /// Display the car information
+            /// </summary>
+
+            public void DisplayInfo()
+            {
+                Console.WriteLine($"Car: {Year} {Make} {Model}");
+            }
+        }
+
+
+    }
